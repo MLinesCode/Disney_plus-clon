bf01830ab0733dfb3f96ca44be42853d33dc3ac8
Specter <mikexx1264@gmail.com>
2021-06-02 02:43:56 -0500

    implementing React-Router / create component Detail - structured

---
+++ src/components/Detail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Detail() {
+  return (
+    <Container>
+      Detail
+    </Container>
+  )
+}
+
+export default Detail
+
+const Container = styled.div`
+
+`
